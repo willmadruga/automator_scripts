cfe42beed6e0d0d903e466c8733c5407edfaf39a
William Madruga <willpaixao@gmail.com>
2010-11-02 01:08:01 -0200

    README file added

--- download_folder_organizer.rb
+++ download_folder_organizer.rb
@@ -1,29 +1,43 @@
-# William :: willpaixao@gmail.com
-# used by mac folder actions
+# Used by mac folder actions
+
 class DownloadFolderOrganizer
-
-  @@download_folder = "$HOME/Downloads"
-
-  # extensão, pasta
-  @@map = Hash[".torrent" => "torrents",
-             ".mov" => "movies",
-             ".jpg" => "images",
-             ".pdf" => "pdfs",
-             ".mp3" => "mp3s",
-             ".dmg" => "apps"
-            ]
+  
+    @@download_folder = "$HOME/Downloads"
+      
+    @@ext_map = Hash[
+               ".torrent" => "torrents",
+               ".mov" => "movies",
+               ".mp4" => "movies",
+               ".flv" => "movies",
+               ".m4v" => "movies",
+               ".avi" => "movies",
+               ".jpg" => "images",
+               ".png" => "images",               
+               ".pdf" => "pdfs",
+               ".mp3" => "mp3s",
+               ".sh" => "apps",
+               ".tar.gz" => "zips",
+               ".zip" => "zips",               
+               ".dmg" => "apps"
+      ]
+           
+    @@pattern_map = Hash[
+                   "(PSP)" => "PSP"  
+      ]       
                  
   def move(file, to)
-    `mv \"#{@@download_folder}/#{file}\" \"#{@@download_folder}/#{to}/\"`
+    origin = "#{@@download_folder}/#{file}"
+    destiny = "#{@@download_folder}/#{to}/"
+    `mv \"#{origin}\" \"#{destiny}\"`
   end
 
   def list_files
     `ls -a #{@@download_folder}`
   end
   
-  def organize
+  def organize_files
     list_files.split("\n").each do |f|
-      @@map.each do |ext,folder|
+      @@ext_map.each do |ext,folder|
         if f.match(ext)
           move(f,folder)
           `growlnotify -m "File #{f} moved to #{folder}"`
@@ -31,8 +45,21 @@ class DownloadFolderOrganizer
       end
     end
   end
-        
+  
+  def organize_folders_by_name
+    # this is hard-coded... any ideas?
+        list_files.split("\n").each do |f|
+          @@pattern_map.each do |pattern,folder|
+            if f.match(pattern)
+              move(f,folder)
+              `growlnotify -m "File #{f} moved to #{folder}"`
+            end
+          end          
+        end
+  end
+          
 end
 
 d = DownloadFolderOrganizer::new
-d.organize
+d.organize_files
+d.organize_folders_by_name
